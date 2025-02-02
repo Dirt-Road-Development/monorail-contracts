3f90a136eb08c1471b26f53e3f220b632e6769a9
TheGreatAxios <sawyer@dirtroad.dev>
2025-02-02 15:00:20 -0800

    Prepare Deployment

--- publish.sh
+++ publish.sh
@@ -8,4 +8,11 @@ cd deployments
 
 npm version minor
 
-npm publish
+version=$(npm pkg get version | sed 's/"//g')
+
+npm publish --access public
+
+cd ../
+
+git add .
+git commit -am "Publish Version $version"
