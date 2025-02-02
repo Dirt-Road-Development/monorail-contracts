871c72359645372caad3340953fe8c60082cf10d
TheGreatAxios <sawyer@dirtroad.dev>
2025-02-02 14:31:53 -0800

    Prepare Deployment

--- publish.sh
+++ publish.sh
@@ -6,10 +6,6 @@ echo "Preparing Publishable Package"
 
 npm version minor
 
-version=$(npm pkg get version)
-
-git checkout -b deployments/$version
-
-gh pr create
+git push
 
 npm publish deployments
